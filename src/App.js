0fc3435ccf984352ad39f080662d7676a3f8b568
Matt Everhart <matt@snthesis.com>
2023-06-17 18:29:20 -0400

    initial commit ofplateholder compliment website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { phraseLibrary} from "./fixtures/phrases";
 
 function App() {
+    const name = 'kate';
+    const personalPhrases = phraseLibrary[name];
+    const finals = personalPhrases.finals;
+    const adjectives = personalPhrases.adjectives;
+    function getRandomInt(max)  {
+        return Math.floor(Math.random() * max);
+    }
+    const num1 = getRandomInt(adjectives.length);
+    let num2 = getRandomInt(adjectives.length);
+    if(num2 === num1) {
+        if(num2 === adjectives.length) {
+            num2--;
+        } else {
+            num2++;
+        }
+    }
+
+    const adjetiveA = adjectives[num1];
+    const adjetiveB = adjectives[num2];
+    const finalPhrase = finals[getRandomInt(finals.length)];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center",}}>
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "80vw", height: "100vh", textAlign:'center'}}>
+            <div style={{marginBottom: 30}}>
+                <span className={'name'}>{name[0].toUpperCase()}{name.slice(1)}{` `}</span>
+                <span className={'generalText'}>you are:</span>
+            </div>
+            <div className={'adj1 generalText'}>{adjetiveA}</div>
+            <div className={"generalText"}>{` `}and{` `}</div>
+            <div className={'adj2 generalText'}>{adjetiveB},</div>
+            <div className={'generalText'}> and {finalPhrase}</div>
+            <div style={{marginTop: 200}}>
+                <button
+                    style={{padding: 30}}
+                    onClick={() => {document.location.reload()}}
+                >
+                    <b>Tell me more nice things about me</b>
+                </button>
+            </div>
+        </div>
+      </div>
   );
 }
 
---
+++ src/fixtures/phrases.js
@@ -0,0 +1,66 @@
+export const phraseLibrary = {
+    kate: {
+        adjectives: [
+            "beautiful",
+            "emotionally intelligent",
+            "caring",
+            "stylish",
+            "buxom",
+            "socially adept",
+            "thoughtful",
+        ],
+        finals: [
+            "Dont you forget it!",
+            "I love you!",
+            "I'll see you soon!",
+            "you got a cute butt!",
+            "that's the law. I dont make the laws",
+        ],
+    },
+    ethan: {
+        adjectives: [
+            "smart",
+            "adaptable",
+            "so F-n tall",
+            "a good lookin' lad",
+            "mature for your age",
+            "gracious beyond your years",
+            "great at mingling with all sorts of people",
+            "no cap, so cool",
+        ],
+        finals: [
+            "keep on rockin!",
+            "you're doing great",
+            "you'll win TI4 one of these days. ;)",
+            "you're making us all proud",
+            "that's the law. I dont make the laws",
+        ],
+    },
+    emily: {
+        adjectives: [
+            "good",
+            "cool",
+        ],
+        finals: [
+           "ill think of something later",
+        ],
+    },
+    ivy: {
+        adjectives: [
+            "good",
+            "cool",
+        ],
+        finals: [
+            "ill think of something later",
+        ],
+    },
+    violet: {
+        adjectives: [
+            "good",
+            "cool",
+        ],
+        finals: [
+            "ill think of something later",
+        ],
+    },
+};
